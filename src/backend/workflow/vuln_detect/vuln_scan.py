05220f20e0d0bd780266c69cf46f56b8fcefa3b7
Jacqueline Tiffany Yeung <jtyeung168@berkeley.edu>
2025-10-30 14:08:22 -0700

    vuln scan cvss fixes
    Was wrongly using the same list object for all the cvss fields, so any updates were getting overwritten by another field. Fixed to use copy.

--- src/backend/workflow/vuln_detect/vuln_scan.py
+++ src/backend/workflow/vuln_detect/vuln_scan.py
@@ -112,7 +112,7 @@ def _extract_grype_df(grype_scan_result):
 		"cvss_v3_base_score": grype_extracted_df['cvss_v3_base_score'] if 'cvss_v3_base_score' in grype_extracted_df.columns else np.nan,
 		"cvss_v3_exploitability_score": grype_extracted_df['cvss_v3_exploitability_score'] if 'cvss_v3_exploitability_score' in grype_extracted_df.columns else np.nan,
 		"cvss_v3_impact_score": grype_extracted_df['cvss_v3_impact_score'] if 'cvss_v3_impact_score' in grype_extracted_df.columns else np.nan,
-		"cvss_v4_vector": grype_extracted_df['cvss_v4_vector'] if 'cvss_v4_vector' in grype_extracted_df.columns else np.nan,
+		"cvss_v4_score": grype_extracted_df['cvss_v4_vector'] if 'cvss_v4_vector' in grype_extracted_df.columns else np.nan,
 		"cvss_v4_version": grype_extracted_df['cvss_v4_version'] if 'cvss_v4_version' in grype_extracted_df.columns else np.nan,
 		"cvss_v4_base_score": grype_extracted_df['cvss_v4_base_score'] if 'cvss_v4_base_score' in grype_extracted_df.columns else np.nan,
 		"cvss_v4_exploitability_score": grype_extracted_df['cvss_v4_exploitability_score'] if 'cvss_v4_exploitability_score' in grype_extracted_df.columns else np.nan,
@@ -191,21 +191,21 @@ def _extract_grype_cvss_df(df: pd.DataFrame) -> pd.DataFrame:
 	num_rows = len(df)
 	nan_array = [np.nan] * num_rows
 	cvss_dict = {
-		"cvss_v2_vector": nan_array,
-		"cvss_v2_version": nan_array,
-		"cvss_v2_base_score": nan_array,
-		"cvss_v2_exploitability_score": nan_array,
-		"cvss_v2_impact_score": nan_array,
-		"cvss_v3_vector": nan_array,
-		"cvss_v3_version": nan_array,
-		"cvss_v3_base_score": nan_array,
-		"cvss_v3_exploitability_score": nan_array,
-		"cvss_v3_impact_score": nan_array,
-		"cvss_v4_vector": nan_array,
-		"cvss_v4_version": nan_array,
-		"cvss_v4_base_score": nan_array,
-		"cvss_v4_exploitability_score": nan_array,
-		"cvss_v4_impact_score": nan_array
+		"cvss_v2_vector": nan_array.copy(),
+		"cvss_v2_version": nan_array.copy(),
+		"cvss_v2_base_score": nan_array.copy(),
+		"cvss_v2_exploitability_score": nan_array.copy(),
+		"cvss_v2_impact_score": nan_array.copy(),
+		"cvss_v3_vector": nan_array.copy(),
+		"cvss_v3_version": nan_array.copy(),
+		"cvss_v3_base_score": nan_array.copy(),
+		"cvss_v3_exploitability_score": nan_array.copy(),
+		"cvss_v3_impact_score": nan_array.copy(),
+		"cvss_v4_vector": nan_array.copy(),
+		"cvss_v4_version": nan_array.copy(),
+		"cvss_v4_base_score": nan_array.copy(),
+		"cvss_v4_exploitability_score": nan_array.copy(),
+		"cvss_v4_impact_score": nan_array.copy()
 	}
 
 	if cvss_metrics is not None:
