a113b49fa17b4ab482dbe44ce292d31ec115467d
Jacqueline Tiffany Yeung <jtyeung168@berkeley.edu>
2025-12-10 16:03:19 -0800

    fix vuln detect provider github PAT

--- src/backend/workflow/vuln_detect/provider.py
+++ src/backend/workflow/vuln_detect/provider.py
@@ -1,6 +1,9 @@
 from src.backend.utils.http import get_json
 import asyncio
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 GITHUB_PERSONAL_ACCESS_TOKEN = os.environ.get("GITHUB_PERSONAL_ACCESS_TOKEN", "")
 
@@ -12,6 +15,7 @@ def fetch_ghsa_details(ghsa_id: str) -> dict:
     }
     if GITHUB_PERSONAL_ACCESS_TOKEN:
         headers["Authorization"] = f"Bearer {GITHUB_PERSONAL_ACCESS_TOKEN}"
+        print("Using GitHub Personal Access Token for authentication.")
 
     ghsa_json = asyncio.run(get_json(ghsa_url, headers=headers))
     return ghsa_json
